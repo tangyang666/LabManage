dbb07a19bc3da853ec9e5d2f6715956bd799be50
yang <tangyang1236@gmail.com>
2019-02-18 20:08:44 +0800

    删除前端模版

--- src/main/java/com/xbn/controller/LabController.java
+++ src/main/java/com/xbn/controller/LabController.java
@@ -2,10 +2,6 @@ package com.xbn.controller;
 
 import com.xbn.pojo.UserInfoPojo;
 import com.xbn.service.LabService;
-import com.xbn.service.impl.LabServiceImpl;
-import net.sf.json.JSON;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
